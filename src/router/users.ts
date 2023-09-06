1b0505ad29e88aab21a4ee5a4582532a1e24d3f9
Val16eria <KStorer61@icloud.com>
2023-09-06 14:06:01 +0300

    refactoring

--- src/router/users.ts
+++ src/router/users.ts
@@ -26,7 +26,7 @@ export default (router: express.Router) => {
 
             const controller = new UserController();
             const response = await controller.getUserById(id, descriptionUser);
-            return res.status(200).send(descriptionUser).end();
+            return res.status(200).send(response).end();
         }
         catch (err) {
             return res.status(400).send({
